4329dd5c6726045dfc840ce4384e3f9aa6adb632
ArseneG <79965932+ArseneG@users.noreply.github.com>
2021-08-06 14:37:33 +0200

    Add files via upload

---
+++ Formulaire_Fournisseurs.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Formulaire_Fournisseur</title>
+</head>
+<body>
+	<form>
+		<label for="FormName"> Nom de Famille :</label><br>
+		<input type="text" id="FormName" name="FormName"><br>
+		<label for="FormFname">Prénom :</label><br>
+		<input type="text" id="FormFname" name="FormFname"><br>
+		<label for="ForMail"> Email :</label><br>
+		<input type="email" id="ForMail" name="ForMail"><br>
+		<label for="FormVille"> Ville :</label><br>
+		<input type="text" id="FormVille" name="FormVille"><br>
+		<label for="FormCp">Code Postal :</label><br>
+		<input type="number" id="FormCp" name="FromCp" min="01000" max="99999"><br>
+		<label for="FormAdresse">Adresse :</label><br>
+		<input type="text" id="FormAdresse" name="FormAdresse"><br>
+		<label for="Password">Choisis ton Mot de Passe :<label><br>
+		<input type="password" id="Password" name="Password"><br>
+		<label for="Vpassword">Verifis ton Mot de Passe :</label><br>
+		<input type="password" id="Vpassword" name="Vpassword"><br>
+		<label for="CodeComptable">Code Comptable :</label><br>
+		<input type="text" id="CodeComptable" name="CodeComptable">
+		<label>Enregistrer</label>
+		<input type="submit" name="submit">
+	</form>
+
+</body>
+</html>
