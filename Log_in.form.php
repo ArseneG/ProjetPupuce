58d8b17c059d78b68b943a0abd7a9fc100e52a0e
ArseneG <79965932+ArseneG@users.noreply.github.com>
2021-09-09 16:31:23 +0200

    Add files via upload

---
+++ Log_in.form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Connectez-vous</title>
+</head>
+<body>
+<form action="Log_in.php" method="post">
+	<label for="ForMail"> Email :</label><br>
+	<input type="email" key="ForMail" name="ForMail"><br>
+	<label for="Password">Choisis ton Mot de Passe :</label><br>
+	<input type="password" key="Password" name="Password"><br>
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
